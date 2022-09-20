9c3239748a70c1f607057c6151e315c5d67383de
MimiMari <marieangele@gmx.de>
2022-09-20 19:53:22 +0200

    added annotation crossorigin

--- src/main/java/de/marie/eventboard/controller/EventController.java
+++ src/main/java/de/marie/eventboard/controller/EventController.java
@@ -2,14 +2,12 @@ package de.marie.eventboard.controller;
 
 import de.marie.eventboard.model.Event;
 import de.marie.eventboard.repository.EventRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class EventController {
 
     private final EventRepository eventRepository;
