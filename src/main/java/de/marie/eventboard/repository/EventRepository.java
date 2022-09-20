682b6623a78555505c9b5aa18d46349765e7aa43
MimiMari <marieangele@gmx.de>
2022-09-20 17:38:28 +0200

    EventRepository added

---
+++ src/main/java/de/marie/eventboard/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package de.marie.eventboard.repository;
+
+import de.marie.eventboard.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
