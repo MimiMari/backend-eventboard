7ac0c3ec9e021459630a340ff79eafcad9d1a57f
MimiMari <marieangele@gmx.de>
2022-09-22 21:22:58 +0200

    code clean up EventServiceTest

--- src/test/java/de/marie/eventboard/service/EventServiceTest.java
+++ src/test/java/de/marie/eventboard/service/EventServiceTest.java
@@ -5,34 +5,38 @@ import de.marie.eventboard.model.Event;
 import de.marie.eventboard.repository.EventRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class EventServiceTest {
 
+    Mapper mapper = new Mapper();
+    private LocalDateTime date = LocalDateTime.of(2022, 9, 25, 11, 53, 00);
 
+    private Event createTestEvent() {
+        Event event = new Event();
+        event.setTitle("Party");
+        event.setDate(LocalDateTime.of(2022, 9, 25, 11, 53, 00));
+        event.setPlace("Hamburg");
+        event.setDescription("Feiern und Tanzen");
+        return event;
+    }
 
-    @Test
-    void getAllEvents() {
-        Mapper mapper = new Mapper();
-
-        LocalDateTime date = LocalDateTime.of(2022, 9, 25, 11, 53, 00);
-
+    private EventDto createTestEventDto() {
         EventDto eventDto = new EventDto();
         eventDto.setTitle("Party");
         eventDto.setDate(String.valueOf(date));
         eventDto.setPlace("Hamburg");
         eventDto.setDescription("Feiern und Tanzen");
+        return eventDto;
+    }
 
+    @Test
+    void getAllEvents() {
         EventRepository eventRepository = Mockito.mock(EventRepository.class);
         EventService eventService = new EventService(eventRepository, mapper);
 
-        eventService.createEvent(eventDto);
+        eventService.createEvent(createTestEventDto());
         eventService.getAllEvents();
 
         Mockito.verify(eventRepository).findAll();
@@ -40,25 +44,11 @@ class EventServiceTest {
 
     @Test
     void createEvent() {
-        Mapper mapper = new Mapper();
-
-        Event event = new Event();
-        event.setTitle("Party");
-        event.setDate(LocalDateTime.of(2022, 9, 25, 11, 53, 00));
-        event.setPlace("Hamburg");
-        event.setDescription("Feiern und Tanzen");
-
-        EventDto eventDto = new EventDto();
-        eventDto.setTitle("Party");
-        eventDto.setDate(String.valueOf(event.getDate()));
-        eventDto.setPlace("Hamburg");
-        eventDto.setDescription("Feiern und Tanzen");
-
         EventRepository eventRepository = Mockito.mock(EventRepository.class);
         EventService eventService = new EventService(eventRepository, mapper);
 
-        eventService.createEvent(eventDto);
+        eventService.createEvent(createTestEventDto());
 
-        Mockito.verify(eventRepository).save(event);
+        Mockito.verify(eventRepository).save(createTestEvent());
     }
 }
