a5eab9b9b84be481d8af43766ef3fb895903d269
MimiMari <marieangele@gmx.de>
2022-09-22 20:58:39 +0200

    equals method added

--- src/main/java/de/marie/eventboard/dto/EventDto.java
+++ src/main/java/de/marie/eventboard/dto/EventDto.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 public class EventDto {
@@ -11,7 +13,18 @@ public class EventDto {
     private String title;
     private String place;
     private String description;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private String date;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EventDto eventDto = (EventDto) o;
+        return Objects.equals(title, eventDto.title) && Objects.equals(place, eventDto.place) && Objects.equals(description, eventDto.description) && Objects.equals(date, eventDto.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, place, description, date);
+    }
 }
--- src/main/java/de/marie/eventboard/model/Event.java
+++ src/main/java/de/marie/eventboard/model/Event.java
@@ -1,14 +1,19 @@
 package de.marie.eventboard.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Setter
 @Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "events")
 public class Event {
 
@@ -19,4 +24,19 @@ public class Event {
     private String place;
     private String description;
     private LocalDateTime date;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return Objects.equals(title, event.title) && Objects.equals(place, event.place) && Objects.equals(description, event.description) && Objects.equals(date, event.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, place, description, date);
+    }
+
 }
